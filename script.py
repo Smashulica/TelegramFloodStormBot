3fb2d6dfd03a4fa11186aba670d78270bd56c381
Smashulica <85994708+Smashulica@users.noreply.github.com>
2021-10-13 01:16:22 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,14 +1,14 @@
-def start(bot, update):
-          bot.sendMessage(chat_id=update.message.chat_id , text="Hello World!")
+def start(update, context):
+    context.bot.sendMessage(chat_id=update.message.chat_id, 'Text One')
             
-def leave(bot, update):
+def leave(update, context):
     for i in range(100):
          bot.leaveChat(chat_id=update.message.chat_id)
          sleep(0.01)
  
-def flodd(bot, update):
+def flodd(update, context):
     while leave:
-         bot.sendMessage(chat_id=update.message.chat_id, text="This script is codec by @FloodStorm")
+         context.bot.sendMessage(chat_id=update.message.chat_id, 'Text Two')
          sleep(0.05)
 		 
 #Non toccare questo script se non sai riprogrammarlo, modifica solo i messaggi.
